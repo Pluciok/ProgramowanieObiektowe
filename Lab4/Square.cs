5f3c06f55580e6f1c3e38abfdbeed3b88293bcb0
Michał Pluta <w69994@student.wsiz.edu.pl>
2024-12-05 10:33:12 +0100

    first commit

---
+++ Lab4/Square.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    internal class Square : Shape
+    {
+        public Square(int x): base(x,x) { }
+        protected override double Calc()
+        {
+            return X * X;
+        }
+    }
+
+}
---
+++ Lab4/lab4.cs
@@ -0,0 +1,63 @@
+﻿//zad1
+
+public abstract class Shape
+{
+
+
+    protected int X { get; set; }
+
+    protected int Y { get; set; }  
+
+
+    protected Shape(int x, int y)
+    {
+
+            X = x;
+            Y = y;
+
+    }
+
+    protected abstract double Calc();
+
+}
+
+//zad2
+
+public interface IVehicle 
+{
+
+    void Start();
+
+    void Stop();
+
+    int MaxSpeed
+    {
+
+        get;
+        set;
+    }
+}
+
+public class Samochod : IVehicle
+{
+    public int MaxSpeed { get; set; }
+    string Name { get; set; }
+    public Samochod(int max, string name)
+    {
+
+            MaxSpeed = max;
+
+            Name = name;
+
+    }
+
+    public void Start()
+    {
+        Console.WriteLine("Wystartuj samochód");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("Zatrzymaj samochód");
+    }
+}
